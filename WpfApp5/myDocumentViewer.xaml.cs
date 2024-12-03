fb7c6d9d8a9d716f03481e43329aca4abc63ebbe
4b2g0036@office.stust.edu.tw <4b2g0036@office.stust.edu.tw>
2024-12-03 15:53:28 +0800

    增強文字編輯功能和事件處理程序
    在 myDocumentViewer.xaml 中：
    - 為 boldButton、italicButton 和 underlineButton 增加 Command 屬性。
    - 為 fontColorPicker 增加 SelectedColorChanged 事件處理程序。
    - 為 fontFamilyComboBox 和 fontSizeComboBox 增加 SelectionChanged 事件處理程序。
    - 為 RichTextBox 增加 SelectionChanged 事件處理程序。
    
    在 myDocumentViewer.xaml.cs 中：
    - 設定 fontFamilyComboBox 的預設選項為第 13 項。
    - 設定 fontSizeComboBox 的預設選項為第 3 項。
    - 新增 fontColorPicker_SelectedColorChanged 事件處理程序。
    - 新增 fontFamilyComboBox_SelectionChanged 事件處理程序。
    - 新增 fontSizeComboBox_SelectionChanged 事件處理程序。
    - 新增 rtbEditer_SelectionChanged 事件處理程序。

--- WpfApp5/myDocumentViewer.xaml.cs
+++ WpfApp5/myDocumentViewer.xaml.cs
@@ -30,8 +30,10 @@ namespace WpfApp5
                 fontFamilyComboBox.Items.Add(fontFamily.Source);
             }
             fontFamilyComboBox.SelectedItem = "Arial";
+            fontFamilyComboBox.SelectedIndex = 13;
 
             fontSizeComboBox.ItemsSource = new List<string> { "8", "9", "10", "11", "12", "14", "16", "18", "20", "22", "24", "26", "28", "36", "48", "72" };
+            fontSizeComboBox.SelectedIndex = 3;
         }
 
         private void NewCommand_Executed(object sender, ExecutedRoutedEventArgs e)
@@ -48,6 +50,55 @@ namespace WpfApp5
         private void SaveCommand_Executed(object sender, ExecutedRoutedEventArgs e)
         {
 
+        }
+
+        private void fontColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            fontColor = (Color)e.NewValue;
+            SolidColorBrush fontBrush = new SolidColorBrush(fontColor);
+            if (rtbEditer != null && rtbEditer.Selection != null)
+            {
+                rtbEditer.Selection.ApplyPropertyValue(TextElement.ForegroundProperty, fontBrush);
+            }
+        }
+
+        private void fontFamilyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (fontFamilyComboBox_SelectionChanged != null)
+            {
+                rtbEditer.Selection.ApplyPropertyValue(TextElement.FontFamilyProperty, fontFamilyComboBox.SelectedItem);
+            }
+        }
+
+        private void fontSizeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (fontSizeComboBox.SelectedItem != null)
+            {
+                rtbEditer.Selection.ApplyPropertyValue(TextElement.FontSizeProperty, fontSizeComboBox.SelectedItem.ToString());
+            }
+        }
+
+        private void rtbEditer_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            var property_bold = rtbEditer.Selection.GetPropertyValue(TextElement.FontWeightProperty);
+            boldButton.IsChecked = (property_bold != DependencyProperty.UnsetValue) && (property_bold.Equals(FontWeights.Bold));
+
+            var property_italic = rtbEditer.Selection.GetPropertyValue(TextElement.FontStyleProperty);
+            italicButton.IsChecked = (property_italic != DependencyProperty.UnsetValue) && (property_italic.Equals(FontStyles.Italic));
+            
+            var property_underline = rtbEditer.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+            underlineButton.IsChecked = (property_underline != DependencyProperty.UnsetValue) && (property_underline.Equals(TextDecorations.Underline));
+
+            var property_fontFamily = rtbEditer.Selection.GetPropertyValue(TextElement.FontFamilyProperty);
+                fontFamilyComboBox.SelectedItem = property_fontFamily.ToString();
+
+
+            var property_fontSize = rtbEditer.Selection.GetPropertyValue(TextElement.FontSizeProperty);
+            fontSizeComboBox.SelectedItem = property_fontSize;
+            
+
+            var property_fontColor = rtbEditer.Selection.GetPropertyValue(TextElement.ForegroundProperty);
+                fontColorPicker.SelectedColor = ((SolidColorBrush)property_fontColor).Color;
         }
     }
 }
